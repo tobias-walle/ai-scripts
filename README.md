49deaf5bfc53d3f7ab90bc6d72863c8e48e2b57d
Tobias Walle <tobias.walle@maibornwolff.de>
2024-01-05 16:01:08 +0100

    add ai-chat script

--- README.md
+++ README.md
@@ -49,3 +49,32 @@ The following tools need to be installed for this command:
 - `eza`: To get informations about the file tree
 - `ripgrep`: To search the filesystem for text
 - `git`: To search the filesystem for text in a git repository
+
+## ai-chat
+
+```sh
+ai-chat <chat-file.md>
+```
+
+Chat with the AI and store the chat in a markdown file.
+This will open your `$EDITOR`. Every time you save the file, an answer will be generated and added to the file.
+
+The syntax of the chat is the following:
+
+- `[<role>]:` Marks the start of a new message
+- You can options like `temperature` in the front matter. (TODO)
+
+Example:
+
+```md
+[system]:
+You are a helpful assistant.
+
+[user]:
+What is the time?
+
+[assistant]:
+As an artificial intelligence, I don't have feelings or consciousness, so I don't experience states of being in the way humans do.
+However I'm here and ready to help you.
+How can I assist you today?
+```
---
+++ ai_scripts/bin/ai_chat.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+import argparse
+import os
+from pathlib import Path
+import sys
+import subprocess
+from typing import Dict, List, Optional, TypeVar
+from openai.types.chat import ChatCompletionMessageParam
+from rich.live import Live
+from rich.markdown import Markdown
+from rich.console import Console
+import re
+import mdformat
+
+from ai_scripts.lib.logging import print, print_step
+from ai_scripts.lib.model import OpenAIGPT4TurboModel
+
+
+def main():
+    console = Console()
+    parser = argparse.ArgumentParser(
+        prog="ai-chat",
+        description="Chat with the ai in an markdown file",
+    )
+    parser.add_argument(
+        "file",
+        help="The file to chat in",
+    )
+    args = parser.parse_args()
+    file = Path(args.file)
+
+    model = OpenAIGPT4TurboModel()
+    editor = os.getenv("EDITOR", "vi")
+
+    if not file.exists():
+        file.write_text(
+            f"{format_role('system')}\nYou are a helpful assistant.\n\n{format_role('user')}\n"
+        )
+
+    while True:
+        md = file.read_text()
+        chat = parse_chat(md)
+        last_msg = last_item(chat)
+        if last_msg is not None and last_msg["role"] == "user":
+            console.clear()
+            with Live() as live:
+                md = md.strip() + f"\n\n{format_role('assistant')}\n"
+                stream = model.stream(chat)
+                for token in stream:
+                    md += token
+                    live.update(Markdown(md))
+                live.update("")
+            print(Markdown(md))
+            md = md.strip() + f"\n\n{format_role('user')}\n\n"
+            file.write_text(mdformat.text(md, options={"wrap": 80}))
+            cancel = console.input("\n\n[grey74]Continue? [Y,n]: [/]").lower() == "n"
+            if cancel:
+                break
+        else:
+            result = subprocess.call([editor, file])
+            if result != 0:
+                print(f"Failed to open the editor. Error code: {result}")
+                sys.exit(1)
+
+            md = file.read_text()
+            last_msg = last_item(parse_chat(md))
+            if last_msg is None or last_msg["role"] != "user":
+                print_step("Last message is not of role 'user'. Exiting...")
+                break
+
+
+def parse_chat(md: str) -> List[ChatCompletionMessageParam]:
+    messages: List[Dict[str, str]] = []
+    message: Optional[Dict[str, str]] = None
+    for line in md.strip().splitlines():
+        role = parse_role(line)
+        if role is not None:
+            if message is not None:
+                message["content"] = message["content"].strip()
+                messages.append(message)
+            message = {"role": role, "content": ""}  # type: ignore
+        else:
+            if message is not None:
+                message["content"] += f"{line}\n"
+    if message is not None:
+        messages.append(message)
+
+    messages = [m for m in messages if m["content"] != ""]
+    return messages  # type: ignore
+
+
+def format_role(role: str) -> str:
+    return f"# --- {role} ---"
+
+
+role_regex = re.compile(r"# --- (.+) --- *$")
+
+
+def parse_role(line: str) -> Optional[str]:
+    role_match = role_regex.match(line)
+    if role_match:
+        return role_match.group(1)
+    else:
+        return None
+
+
+T = TypeVar("T")
+
+
+def last_item(list: List[T]) -> Optional[T]:
+    return None if len(list) == 0 else list[-1]
+
+
+if __name__ == "__main__":
+    main()
