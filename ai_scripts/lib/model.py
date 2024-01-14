ac0704f82f4a7df3e6facf97af996956be6bb9f1
Tobias Walle <tobias.walle@maibornwolff.de>
2024-01-14 18:28:25 +0100

    add ollama models

--- ai_scripts/lib/model.py
+++ ai_scripts/lib/model.py
@@ -3,17 +3,29 @@ import os
 from typing import (
     Iterable,
     List,
+    Literal,
     NotRequired,
     Optional,
     Protocol,
+    Required,
     TypedDict,
     Unpack,
 )
+from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHandler
+from langchain_core.language_models import LanguageModelInput
+from langchain_core.language_models.chat_models import BaseChatModel
+from langchain_community.chat_models import ChatOllama
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 from openai import OpenAI
 from openai.types.chat import ChatCompletionMessageParam
 
 from ai_scripts.lib.logging import print_status, print_step
+
+
+class Message(TypedDict):
+    content: Required[str]
+    role: Required[Literal["system"] | Literal["user"] | Literal["assistant"]]
 
 
 class ChatOptions(TypedDict):
@@ -29,14 +41,14 @@ class Model(Protocol):
 
     def complete(
         self,
-        messages: List[ChatCompletionMessageParam],
+        messages: List[Message],
         **kwargs: Unpack[ChatOptions],
     ) -> str:
         ...
 
     def stream(
         self,
-        messages: List[ChatCompletionMessageParam],
+        messages: List[Message],
         **kwargs: Unpack[ChatOptions],
     ) -> Iterable[str]:
         ...
@@ -51,7 +63,10 @@ class OpenAICompatibleModel(Model):
     def complete(self, messages, **kwargs):
         print_status(f"Using {self.name}")
         answer = self.client.chat.completions.create(
-            model=self.name, messages=messages, stream=False, **kwargs
+            model=self.name,
+            messages=self.map_messages(messages),
+            stream=False,
+            **kwargs,
         )
         return answer.choices[0].message.content or ""
 
@@ -59,7 +74,7 @@ class OpenAICompatibleModel(Model):
         print_status(f"Using {self.name} (stream)")
         stream = self.client.chat.completions.create(
             model=self.name,
-            messages=messages,
+            messages=self.map_messages(messages),
             stream=True,
             **kwargs,
         )
@@ -69,44 +84,102 @@ class OpenAICompatibleModel(Model):
             if chunk.choices[0].delta.content is not None
         )
 
-    def _model(self) -> str:
-        ...
+    def map_messages(self, messages: List[Message]) -> List[ChatCompletionMessageParam]:
+        result: List[ChatCompletionMessageParam] = []
+        for m in messages:
+            if m["role"] == "system":
+                result.append({"role": "system", "content": m["content"]})
+            if m["role"] == "user":
+                result.append({"role": "user", "content": m["content"]})
+            if m["role"] == "assistant":
+                result.append({"role": "assistant", "content": m["content"]})
+        return result
 
-    def _client(self) -> OpenAI:
-        ...
+
+class LangchainModel(Model):
+    def __init__(
+        self, name: str, abbr: Optional[str], base_model: BaseChatModel
+    ) -> None:
+        self.name = name
+        self.abbr = abbr
+        self.base_model = base_model
+
+    def complete(self, messages, **kwargs) -> str:
+        print_status(f"Using {self.name}")
+        answer = self.base_model.invoke(self.map_messages(messages), **kwargs)
+        return str(answer.content)
+
+    def stream(self, messages, **kwargs) -> Iterable[str]:
+        print_status(f"Using {self.name} (stream)")
+        stream = self.base_model.stream(self.map_messages(messages), **kwargs)
+        return (str(chunk.content) for chunk in stream)
+
+    def map_messages(self, messages: List[Message]) -> LanguageModelInput:
+        result: LanguageModelInput = []
+        for m in messages:
+            if m["role"] == "system":
+                result.append(SystemMessage(content=m["content"]))
+            if m["role"] == "user":
+                result.append(HumanMessage(content=m["content"]))
+            if m["role"] == "assistant":
+                result.append(AIMessage(content=m["content"]))
+        return result
 
 
-def _mistral_client() -> OpenAI:
-    return OpenAI(
-        api_key=os.getenv("MISTRAL_API_KEY"),
-        base_url="https://api.mistral.ai/v1",
+def openai_model(name: str, abbr: Optional[str]) -> Model:
+    return OpenAICompatibleModel(
+        name,
+        abbr,
+        OpenAI(),
     )
 
 
-def _together_client() -> OpenAI:
-    return OpenAI(
-        api_key=os.getenv("TOGETHER_API_KEY"),
-        base_url="https://api.together.xyz/v1",
+def mistralai_model(name: str, abbr: Optional[str]) -> Model:
+    return OpenAICompatibleModel(
+        name,
+        abbr,
+        OpenAI(
+            api_key=os.getenv("MISTRAL_API_KEY"),
+            base_url="https://api.mistral.ai/v1",
+        ),
     )
 
 
-def _openai_client() -> OpenAI:
-    return OpenAI(
-        api_key=os.getenv("OPENAI_API_KEY"),
+def togetherai_model(name: str, abbr: Optional[str]) -> Model:
+    return OpenAICompatibleModel(
+        name,
+        abbr,
+        OpenAI(
+            api_key=os.getenv("TOGETHER_API_KEY"),
+            base_url="https://api.together.xyz/v1",
+        ),
+    )
+
+
+def ollama_model(model: str, name: str, abbr: Optional[str]) -> Model:
+    return LangchainModel(
+        name,
+        abbr,
+        ChatOllama(
+            base_url="http://localhost:11434",
+            model=model,
+        ),
     )
 
 
 class Models(Enum):
-    GPT_4_TURBO = OpenAICompatibleModel("gpt-4-1106-preview", "G4", _openai_client())
-    MIXTRAL_8_7B = OpenAICompatibleModel(
-        "mistralai/Mixtral-8x7B-Instruct-v0.1", "MX8", _together_client()
+    GPT_4_TURBO = openai_model("gpt-4-1106-preview", "G4")
+    MIXTRAL_8_7B = togetherai_model("mistralai/Mixtral-8x7B-Instruct-v0.1", "M8")
+    MISTRAL_7B = togetherai_model("mistralai/Mistral-7B-Instruct-v0.2", "M7")
+    MISTRAL_TINY = mistralai_model("mistral-tiny", "MT")
+    MISTRAL_SMALL = mistralai_model("mistral-small", "MS")
+    MISTRAL_MEDIUM = mistralai_model("mistral-medium", "MM")
+
+    OLLAMA_MISTRAL_7B = ollama_model(
+        "mistral:7b-instruct",
+        "ollama/mistral:7b-instruct",
+        "OM7",
     )
-    MISTRAL_7B = OpenAICompatibleModel(
-        "mistralai/Mistral-7B-Instruct-v0.2", "MS7", _together_client()
-    )
-    MISTRAL_TINY = OpenAICompatibleModel("mistral-tiny", "MST", _mistral_client())
-    MISTRAL_SMALL = OpenAICompatibleModel("mistral-small", "MSS", _mistral_client())
-    MISTRAL_MEDIUM = OpenAICompatibleModel("mistral-medium", "MSM", _mistral_client())
 
     @classmethod
     def get_from_env_or_default(cls, default_model: Optional[Model] = None) -> Model:
@@ -127,4 +200,4 @@ class Models(Enum):
         print_step(
             f'Couldn\'t find model "{name}". Try to access it anyway on TogetherAI.'
         )
-        return OpenAICompatibleModel(name, None, _together_client())
+        return togetherai_model(name, None)
